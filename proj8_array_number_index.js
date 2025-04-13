aa8305b6c811c66684da8ca5a01a94b185d1ee75
Plang Gabriel Damwesh <plang.gabriel@gmail.com>
2025-04-13 11:07:06 +0100

    upgrades proj8_array_number_index.js to eliminate the built-in findIndex() function

--- proj8_array_number_index.js
+++ proj8_array_number_index.js
@@ -1,11 +1,13 @@
-const number = [3, 10, 18, 20, 35, 40, 45, 49, 35, 50, 49];
+const arrayContainer = [3, 10, 18, 20, 35, 40, 45, 49, 35, 50, 49];
 
-let index = 0
+function findIndex(num, arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === num) {
+            return console.log(`The index of the number ${num} is ${i}`)
+        }
+    }
+    return console.log(`The number ${num} does not exist in this array => [${arr}]`)
 
-function findNumber(number) {
-    return number == 49;
 }
 
-index = number.findIndex(findNumber)
-
-console.log(`The index of the number is ${index}`)
+findIndex(52, arrayContainer)
