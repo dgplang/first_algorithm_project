8010da641eaa00559ee931d809c866985ff7e1f5
Plang Gabriel Damwesh <plang.gabriel@gmail.com>
2025-04-13 12:34:46 +0100

    upgrades proj9_arrays_merge_sorting.js eliminates concat() and sort() methods replacing them with spread operator and bubble sort algorithm implementation respectively

--- proj9_arrays_merge_sorting.js
+++ proj9_arrays_merge_sorting.js
@@ -1,6 +1,38 @@
-function mergeSortFxn(arr1, arr2, order) {
+function sortFn(numArr, ord) {
+    if (order === "asc") {
+        do {
+            swapped = false
+            for (i = 0; i < numArr.length - 1; i++) {
+                if (numArr[i] > numArr[i + 1]) {
+                    temp = numArr[i]
+                    numArr[i] = numArr[i + 1]
+                    numArr[i + 1] = temp
+                    swapped = true
+                }
+            }    
+        } while (swapped)
+            console.log(`The merged & sorted array in ascending order is ${numArr}`)
+    } else if ( order === "desc") {
+        do {
+            swapped = false
+            for (i = 0; i < numArr.length - 1; i++) {
+                if (numArr[i] < numArr[i + 1]) {
+                    temp = numArr[i]
+                    numArr[i] = numArr[i + 1]
+                    numArr[i + 1] = temp
+                    swapped = true
+                }
+            }    
+        } while (swapped)
+            console.log(`The merged & sorted array in descending order is ${numArr}`)
+    }
+}
+
+function mergeSortFxn(arr1, arr2, order1) {
+
+    order = order1.toLowerCase()
     
-    merged = arr1.concat(arr2);
+    merged = [...arr1, ...arr2];
 
     console.log(`The first array passed is ${arr1} and the second array passed is ${arr2}`)
 
@@ -8,16 +40,11 @@ function mergeSortFxn(arr1, arr2, order) {
 
     direction = order.toLowerCase()
 
-    if (direction === "asc") {
-        merged.sort(function(a, b) {return a - b});
-        console.log(`The sorted merged array in ascending order is ${merged}`)
-    } else if (direction === "desc") {
-        merged.sort(function(a, b) {return b - a});
-        console.log(`The sorted merged array in descending order is ${merged}`)
-    }
+    sortFn(merged, order)
+
 }
 
 let arrPassed1 = [2, 5, 8, 23, 63, 1, 3]
 let arrPassed2 = [3, 4, 75, 35, 20]
 
-mergeSortFxn(arrPassed1, arrPassed2, "ASC")
+mergeSortFxn(arrPassed1, arrPassed2, "desc")
